54363d6ebf4e083a21f194741bd49881169ae0fb
petrishinvlad <petrishin.vladislav@gmail.com>
2024-12-18 00:06:16 +0000

    Initializing the project
    Fixing the database local config for postgres 13

--- NutritionApi/settings.py
+++ NutritionApi/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'NutritionApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nutrition',
+        'USER': 'core',
+        'PASSWORD': 'WhYNKjmawdmdpaw122129$$$&&&&',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
