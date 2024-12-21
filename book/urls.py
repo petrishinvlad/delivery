7d9610cdd2593f63b7582f2927c9bb000d1de8d3
petrishinvlad <petrishin.vladislav@gmail.com>
2024-12-21 18:36:38 +0000

    Dynamic urls

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from book import views
 
+app_name = 'book'
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:age>/', views.show_age, name='show_age')
+    path('/', views.index, name='index'),
+    path('/<int:age>/', views.show_age, name='show_age'),
+    path('/name/<str:name>/', views.show_name, name='show-name')
 ]
--- book/views.py
+++ book/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 def show_age(request, age):
     return HttpResponse("I am %s years old." % age)
 
+def show_name(request, name):
+    return HttpResponse("Hello, I am showing the name - %s" % name)
+
 def index(request):
     my_dict = {'insert_me': "Hello I am from views.py!"}
     return render(request, 'book/index.html', context=my_dict)
