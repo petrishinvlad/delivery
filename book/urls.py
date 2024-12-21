f379e055ce6974dd1ebf9388d9b871f0ae1633d5
petrishinvlad <petrishin.vladislav@gmail.com>
2024-12-21 20:33:09 +0000

    Template inheritance example

--- book/urls.py
+++ book/urls.py
@@ -4,7 +4,10 @@ from book import views
 app_name = 'book'
 
 urlpatterns = [
-    path('/', views.index, name='index'),
-    path('/<int:age>/', views.show_age, name='show_age'),
-    path('/name/<str:name>/', views.show_name, name='show-name')
+    path('', views.index, name='index'),
+    path('<int:age>/', views.show_age, name='show_age'),
+    path('name/<str:name>/', views.show_name, name='show-name'),
+    path('home/', views.home, name="home"),
+    path('search/', views.search, name="search"),
+    path('about/', views.about, name="about"),
 ]
--- book/views.py
+++ book/views.py
@@ -10,3 +10,12 @@ def show_name(request, name):
 def index(request):
     my_dict = {'insert_me': "Hello I am from views.py!"}
     return render(request, 'book/index.html', context=my_dict)
+
+def home(request):
+    return render(request, 'book/home.html')
+
+def search(request):
+    return render(request, 'book/search.html')
+
+def about(request):
+    return render(request, 'book/about.html')
